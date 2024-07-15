4d6c6c4df26f5c55e8d6c5fc1b6101656cad9949
sk790 <saurabhk2890@gmail.com>
2024-07-15 13:29:31 +0530

    created api for top 10 laptop  and ctegory laptop and company name AMZ and min price 1 max price 10000 and also added solution images

--- Question1/apiRoute.js
+++ Question1/apiRoute.js
@@ -5,5 +5,6 @@ import { register } from "./controller.js"
 const router = express.Router()
 
 router.route('/test/register').post(register)
+router.route('/test/companies/:companyname/categories/:categoryname/products?top=n&minPrice=p&maxPrice=q').post(register)
 
 export default router
--- Question1/controller.js
+++ Question1/controller.js
@@ -1,5 +1,23 @@
 export const register = async(req, res) => {
-    
 
-    res.send("Hello")
+    res.send("Hello World")
+}
+
+export const getProduct = async(req, res) => {
+    
+  const companyName = req.params.companyname;
+  const categoryName = req.params.categoryname;
+
+  const topN = req.query.top;
+  const minPrice = req.query.minPrice; 
+  const maxPrice = req.query.maxPrice; 
+
+  res.json({
+    company: companyName,
+    category: categoryName,
+    top: topN,
+    minPrice: minPrice,
+    maxPrice: maxPrice
+  });
+
 }
