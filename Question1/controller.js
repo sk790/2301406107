19e9a9ddd6eb19309a0601a295b7f9e1c555bb04
sk790 <saurabhk2890@gmail.com>
2024-07-15 13:37:55 +0530

    created controller for auth and register routes

--- Question1/controller.js
+++ Question1/controller.js
@@ -1,6 +1,21 @@
 export const register = async(req, res) => {
+    try {
+        res.send(res.body)
+    } catch (error) {
+        res.json({msg:error})
+    }
+}
 
-    res.send("Hello World")
+export const auth = async(req, res) => {
+    const {access_token} = req.body
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIxMDMwMjQ2LCJpYXQiOjE3MjEwMjk5NDYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjUwNDc1ZjUwLTc0YmMtNDY5NC1hODdkLTI0MDAzM2Q4N2Y4MiIsInN1YiI6ImFrc2hheUBhYmMuZWR1In0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiI1MDQ3NWY1MC03NGJjLTQ2OTQtYTg3ZC0yNDAwMzNkODdmODIiLCJjbGllbnRTZWNyZXQiOiJLZnVyaGx4TkV4T2tQRUNKIiwib3duZXJOYW1lIjoiQWtzaGF5Iiwib3duZXJFbWFpbCI6ImFrc2hheUBhYmMuZWR1Iiwicm9sbE5vIjoiMSJ9.tzejXvk-zc1gF913dZUCHXCROmLYolnQebxELW_2sqc"
+    try {
+        if(access_token === token){
+            res.send(res.body)
+        }
+    } catch (error) {
+        res.json({msg:error})
+    }
 }
 
 export const getProduct = async(req, res) => {
@@ -19,5 +34,4 @@ export const getProduct = async(req, res) => {
     minPrice: minPrice,
     maxPrice: maxPrice
   });
-
 }
